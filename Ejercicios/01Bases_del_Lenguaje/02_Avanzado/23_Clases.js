348d0340565dd7385358d79113f08d44b8f72da0
LechuDevX <jpixi8@gmail.com>
2026-06-30 00:45:08 -0600

    Mejoras

--- Ejercicios/01Bases_del_Lenguaje/02_Avanzado/23_Clases.js
+++ Ejercicios/01Bases_del_Lenguaje/02_Avanzado/23_Clases.js
@@ -2,6 +2,9 @@
 //Es una plantilla para crear objetos
 //Se define con la palabra clave class y el nombre de la clase
 //requiere de un funcion o metodo constructor para inicializar su estructura basica
+//El metodo costructor es el primero que se ejecuta al crear un objeto
+//Todas los metodos o funciones pueden tener propiedades con valores por defecto,
+//Privadas o publicas
 
 class Persona {
     constructor(nombre = "User", edad = 0) {
@@ -18,13 +21,15 @@ class Persona {
 let persona = new Persona('Ana', 25);
 var persona2 = new Persona('Lechu', 29)
 
+//Acceder a propiedades publicas por punto (.)
 console.log(persona.nombre); // Ana
 console.log(persona.edad); // 25
 
-console.log(persona2.nombre); // Lechu
-console.log(persona2.edad); // 29
+//Acceder a variables publicas con []
+console.log(persona2['nombre']); // Lechu
+console.log(persona2['edad']); // 29
 
-//Si imprimimos el tipo de dasto que son no retorna que son objetos
+//Si imprimimos el tipo de dasto que son nos retorna que son objetos
 console.log(typeof persona)// objet
 console.log(typeof persona2)//object
 // Llamada a metodos
